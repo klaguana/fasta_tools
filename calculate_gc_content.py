079ffb9d00b522f9aefee49d6e064b2383ef49c5
klaguana <klaguana@hawaii.edu>
2015-10-14 21:34:24 -1000

    added percentages

--- calculate_gc_content.py
+++ calculate_gc_content.py
@@ -2,7 +2,9 @@
 
 # Command line script to calculate base frequency from a .fasta file
 
+from __future__ import division
 import sys
+
 
 a_count = 0
 c_count = 0
@@ -14,11 +16,12 @@ with open(sys.argv[1], 'r') as fasta:
         if line.startswith(">"):
             # Header line, skip it
             continue
-        
-        a_count += line.count("A")
-	c_count += line.count("C")
-        g_count += line.count("G")
-	t_count += line.count("T")
+        else:
+            a_count += line.count("A")
+	    c_count += line.count("C")
+            g_count += line.count("G")
+	    t_count += line.count("T")
+	    p = a_count + c_count + g_count + t_count
    #    for base in line.strip():
    #         if base == "A":
    #             a_count += 1
@@ -31,6 +34,10 @@ with open(sys.argv[1], 'r') as fasta:
 
 print("Base counts for file %s:" % sys.argv[1])
 print("A: %d" % a_count)
+print '{:.0%}'.format(a_count/p)
 print("C: %d" % c_count)
+print '{:.0%}'.format(c_count/p)
 print("G: %d" % g_count)
+print '{:.0%}'.format(g_count/p)
 print("T: %d" % t_count)
+print '{:.0%}'.format(t_count/p)
